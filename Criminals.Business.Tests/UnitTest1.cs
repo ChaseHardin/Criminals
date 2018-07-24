a9c463c557a42882aabdd79b076375407752d8f4
Chase Hardin <chardin@pillartechnology.com>
2018-07-24 16:19:44 -0500

    Add testing projects for API and Business

---
+++ Criminals.Business.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Criminals.Business.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
